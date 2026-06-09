494917d84715617eacb1fb4221c56c438c9d9e40
Liga <ligaasike@gmail.com>
2026-06-09 16:52:13 +0300

    more sqlite queries I tried out

--- queries.sql
+++ queries.sql
@@ -1,2 +1,16 @@
 SELECT * FROM teams
 WHERE country = 'LV';
+
+SELECT * FROM players
+WHERE game = 'CS2';
+
+SELECT
+    p.nickname,
+    t.name
+FROM players p
+JOIN teams t
+ON p.active_team_id = t.id;
+
+SELECT *
+FROM tournaments
+WHERE prize_pool > 10000;
