cb9a8e7f81297ab88373d1dab96840b9d4a58f84
Liga <ligaasike@gmail.com>
2026-06-09 17:50:58 +0300

    Add SQL queries for data analysis

--- queries.sql
+++ queries.sql
@@ -14,3 +14,51 @@ ON p.active_team_id = t.id;
 SELECT *
 FROM tournaments
 WHERE prize_pool > 10000;
+
+--------------------------------------
+-- All Latvian teams
+
+SELECT *
+FROM teams
+WHERE country = 'LV';
+
+------------------------------------------------
+
+-- All CS2 players
+
+SELECT *
+FROM players
+WHERE game = 'CS2';
+
+------------------------------------------------
+
+-- Players and their teams
+
+SELECT
+    p.nickname,
+    p.real_name,
+    t.name AS team_name
+FROM players p
+JOIN teams t
+ON p.active_team_id = t.id;
+
+------------------------------------------------
+
+SELECT
+    game,
+    COUNT(*) AS team_count
+FROM teams
+GROUP BY game;
+
+
+SELECT *
+FROM tournaments
+WHERE prize_pool > 1000;
+
+SELECT
+    tp.place_achieved,
+    tp.prize_won,
+    t.name AS team_name
+FROM tournament_placements tp
+JOIN teams t
+ON tp.team_id = t.id;
---
+++ query_results.md
@@ -0,0 +1,9 @@
+# SQL Query Testing
+
+## Tested Queries
+
+- Team filtering by country
+- Player filtering by game
+- Player and team JOIN query
+- Tournament filtering by prize pool
+- Tournament placement analysis
